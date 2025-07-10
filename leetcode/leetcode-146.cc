dfb44fc5c36743236387c4a7cd10cbce1f004729
carlvinhust2012 <huchenghappy@126.com>
2025-07-10 15:20:21 +0800

    add some leetcode item

---
+++ leetcode/leetcode-146.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+#include <unordered_map>
+
+class LRUcace {
+public:
+   LRUcace(int cap) : capcity(cap) {}
+   int put(int key, int val) {}
+   int get (int key) {}
+private:
+   int capcity;
+};
+
+int main() {
+    return 0;
+}
